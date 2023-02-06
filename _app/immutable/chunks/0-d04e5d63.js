f3feb0aa376a3ebc9a4fd743a3a22de24759fa9b
Samuel Lang <langsamuel@web.de>
2023-02-06 15:18:20 +0100

    Updates

---
+++ _app/immutable/chunks/0-d04e5d63.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-6e7f5913.js";export{t as component};
---
+++ _app/immutable/chunks/1-0dd72c0a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-53d01bde.js";export{t as component};
